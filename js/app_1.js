74d84372632321cf920c6b81d0002af036f7aa35
Cat McGuinness <maiamcguinness@gmail.com>
2014-05-07 00:02:42 -0500

    Renamed the app to align with github, heh. Added equals functionality. Fixed addition, made it functional!! Fixed derping about with the resetters.

--- js/app_1.js
+++ js/app_1.js
@@ -4,6 +4,7 @@ app_1.controller('CalcCtrl', function($scope){
     $scope.current_entry = ['0']; // initializing to nothing
     $scope.stored_entry = ['0']; // initializing to nothing
     $scope.tally = 0; // haven't done any math yet!
+    $scope.operation = ''; // haven't done any math yet!
 
     $scope.number_push = function(entered_entry) {
         if ($scope.current_entry[0] === '0') {
@@ -27,9 +28,14 @@ app_1.controller('CalcCtrl', function($scope){
         $scope.stored_entry[0] = '0';
     }
 
+    $scope.clear_tally = function() {
+        $scope.tally = 0;
+    }
+
     $scope.clear_all = function() {
         $scope.clear_current();
         $scope.clear_stored();
+        $scope.clear_tally();
     }
 
     $scope.clean_stored_entry = function() {
@@ -49,14 +55,32 @@ app_1.controller('CalcCtrl', function($scope){
     }
 
     $scope.plus = function() {
+        if ($scope.operation === 'equals') {
+            $scope.clear_stored();
+        }
+
         $scope.clean_stored_entry();
 
         for (var i = 0; i < $scope.current_entry.length; i++) {
             $scope.stored_entry.push($scope.current_entry[i]);
         }
-        $scope.tally += $scope.stored_entry.join('');
-        $scope.stored_entry.push(' + ');
+        $scope.tally += parseFloat($scope.current_entry.join(''));
 
+        console.log($scope.tally)
+        $scope.stored_entry.push(' + ');
         $scope.clear_current();
+        $scope.operation = 'plus';
+    }
+
+    $scope.equals = function() {
+        if ($scope.operation === 'plus') {
+            $scope.plus();
+        }
+
+        $scope.operation = 'equals';
+        $scope.clear_stored();
+        $scope.stored_entry[0] = $scope.tally;
+
+        $scope.clear_tally();
     }
 });
